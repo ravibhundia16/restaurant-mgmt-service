be9ced9c318a131a57d5945d697a56bac3dbfe2b
Ravi Bhundia <ravib@auberginesolutions.com>
2021-10-31 10:35:55 +0530

    refactor: add comments

--- src/app.js
+++ src/app.js
@@ -12,13 +12,13 @@ const cors = require('cors')
 
 let app = express();
 
-// Intialize DB Connection
+// * Intialize DB Connection
 const dbUtil = require('./middleware/db')(config.database)
 dbUtil.connect()
 
 global.db = dbUtil
 
-// view engine setup
+// * view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -38,18 +38,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(urlCons.PARAM_API_PREFIX, restaurantDataRoute)
 app.use(urlCons.PARAM_API_PREFIX, insertDataIntoDbRoute)
 
-// catch 404 and forward to error handler
+// * catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
+// * error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
+  // * set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // * render the error page
   res.status(err.status || 500);
   res.render('error');
 });
